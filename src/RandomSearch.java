3d81fbeb9ac2611b6c98dbce833e06fb2bb6a0b0
robotics <robotics@10.0.2.54>
2014-12-03 12:46:23 -0800

    Added bare RandomSearch behavior

---
+++ src/RandomSearch.java
@@ -0,0 +1,23 @@
+import lejos.robotics.subsumption.Behavior;
+
+
+public class RandomSearch implements Behavior {
+
+	@Override
+	public boolean takeControl() {
+		return true;
+	}
+
+	@Override
+	public void action() {
+
+
+	}
+
+	@Override
+	public void suppress() {
+
+
+	}
+
+}
